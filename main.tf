273a38854fcf18277620a470438a99225c9f173b
George Rodrigues <georgerodrigues@live.com.au>
2022-04-17 20:50:24 -0300

    feat: create tls key

--- main.tf
+++ main.tf
@@ -12,6 +12,16 @@ terraform {
   }
 }
 
+resource "tls_private_key" "tls_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "key_pair_sobral" {
+  key_name   = "my_super_key"
+  public_key = tls_private_key.tls_key.public_key_openssh
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
