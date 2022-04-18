a4bed35a89caa9a23eb739cb2e820a6aa13d339a
George Rodrigues <georgerodrigues@live.com.au>
2022-04-18 12:48:55 -0300

    feat: create security group to allow ssh

--- main.tf
+++ main.tf
@@ -22,12 +22,36 @@ resource "aws_key_pair" "key_pair_sobral" {
   public_key = tls_private_key.tls_key.public_key_openssh
 }
 
-#TODO: create security group to connect ssh
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  key_name      = aws_key_pair.key_pair_sobral.key_name
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.key_pair_sobral.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "sobral-lab"
