c4f8aff9b0d59d56838085df50af9cbd20a9f96c
edsoncelio <edsoncelio2020@gmail.com>
2022-04-18 18:32:45 -0300

    feat: refactor folders structure

---
+++ demo-webservice/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-sobral-devops-lab-tf-state"
+    key    = "terraform-test.tfstate"
+    region = "us-east-2"
+  }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
